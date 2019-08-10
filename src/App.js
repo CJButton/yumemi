d1ec6cd776695afb611ea453cb72143d590220fc
CJButton <gsyme1907@gmail.com>
2019-08-10 15:14:57 +0900

    moved basic highcharts structure to another file

--- src/App.js
+++ src/App.js
@@ -9,28 +9,14 @@ import logo from './logo.svg';
 import { fetchPrefectures,
          fetchPrefecturePopulation } from './requests'
 import RenderCheckboxes from './RenderCheckboxes'
-
-// const defaultChartOptions = {
-//       chartOptions: {
-//         credits: {
-//           enabled: true,
-//           text: 'Created by Christopher Button 2019'
-//           },
-//           title: { text: '', align: 'center', },
-//         legend: {
-//           layout: 'vertical',
-//           align: 'right',
-//           verticalAlign: 'middle'
-//         },
-//         yAxis: { title: { text: '総人口' }},
-//       }
-// }
+import ChartOptions from './HighChartsOptions'
 
 const App = () => {
   const [chartOptions, updateChart] = useState({})
-  const [isLoading, updateLoader] = useState(true)
+  const [isLoading, updateLoader] = useState(false)
   const [checkedPrefs, updateCheckedPrefs] = useState({}) // { prefName: bool }
   const [prefArray, updatePrefArray] = useState([]) // [ { prefName: str, prefCode: num } ]
+  const [initialLoad, updateInitialLoad] = useState(true)
   const chartRef = useRef();
 
     // title: {
@@ -43,6 +29,7 @@ const App = () => {
   }
 
   const removeFromChart = (idx, prefName) => {
+    updateLoader(true)
     const series = JSON.parse(JSON.stringify(chartOptions.series))
     series.splice(idx, 1)
     const updatedChartOptions = {
@@ -51,9 +38,11 @@ const App = () => {
     }
     updateCheckedPrefs({...checkedPrefs, [prefName]: false })
     updateChart(updatedChartOptions)
+    updateLoader(false)
   }
 
   const getPrefecturePopulation = async ({prefName, prefCode}) => {
+    updateLoader(true)
     const prefStatus = verifyIfPrefectureIsDisplayed(prefCode)
     if(prefStatus > -1) return removeFromChart(prefStatus, prefName)
 
@@ -73,78 +62,74 @@ const App = () => {
     }
     updateChart(updatedChartOptions)
     updateCheckedPrefs({...checkedPrefs, [prefName]: true })
+    updateLoader(false)
+  }
+
+  const errorHandler = (err) => {
+    console.error('oh no! an Error')
+    console.error(err.message)
   }
 
   useEffect(() => {
     const initialFetch = async () => {
       try {
         const res = await fetchPrefectures()
-        // if(res.status && res.status >= 300) 
-        // description: ""
-        // message: "Forbidden."
-        // statusCode: "403"
-          // const randPref = Math.floor(Math.random() * Math.floor(res.result.length))
-          const { prefCode, prefName } = res.result[46]
-          // we randomly fetch one of the prefecures data using their prefCode
-          const popData = await fetchPrefecturePopulation(prefCode)
-          const prefGeneralPop = popData.result.data[0].data.map(el => el.value)
-          const years = popData.result.data[0].data.map(el => el.year)
-          const prefCodes = res.result.reduce((acc, pref) => ({ ...acc, [pref.prefName]: false}), {})
-          updateLoader(false)
-          updateCheckedPrefs({ ...prefCodes, [prefName]: true })
-          updatePrefArray(res.result)
+        if(res.statusCode) throw new Error()
+        
+        const randPref = Math.floor(Math.random() * Math.floor(res.result.length))
+        const { prefCode, prefName } = res.result[randPref]
 
-          const updatedChartOptions = {
-            credits: {
-              enabled: true,
-              text: 'Created by Christopher Button 2019'
-              },
-              title: { text: '', align: 'center', },
-            legend: {
-              layout: 'vertical',
-              align: 'right',
-              verticalAlign: 'middle'
-            },
-            yAxis: { title: { text: '総人口' }},
-            xAxis: { categories: years },
-            series: [ 
-              { prefCode, 
-                name: prefName,
-                data: prefGeneralPop } 
-            ],
-          }
-          updateChart(updatedChartOptions)
-  
-          const container = chartRef.current.container.current
-          container.style.width = "100%";
-          chartRef.current.chart.reflow();
-        }
-        catch(err) {
-          console.log(err, 'oh no! error!')
-        }
+        // we randomly fetch one of the prefecures data using their prefCode
+        const popData = await fetchPrefecturePopulation(prefCode)
+        if(popData.statusCode) throw new Error()
+        
+        const prefGeneralPop = popData.result.data[0].data.map(el => el.value)
+        const years = popData.result.data[0].data.map(el => el.year)
+        const prefCodes = res.result.reduce((acc, pref) => ({ ...acc, [pref.prefName]: false}), {})
+        updateInitialLoad(false)
+        updateCheckedPrefs({ ...prefCodes, [prefName]: true })
+        updatePrefArray(res.result)
+
+        const initialChart = JSON.parse(JSON.stringify(ChartOptions))
+        initialChart.yAxis = { title: { text: '総人口' }}
+        initialChart.xAxis = { categories: years }
+        initialChart.series.push(
+          { prefCode, 
+            name: prefName,
+            data: prefGeneralPop } )
+        updateChart(initialChart)
+
+        const container = chartRef.current.container.current
+        container.style.width = "100%";
+        chartRef.current.chart.reflow();
+      }
+      catch(err) {
+        errorHandler(err)
+      }
     }
     initialFetch()
   }, [])
     return(
       <div className="App">
         <header className="App-header">
-        { isLoading && <img src={logo} className="App-logo" alt="logo" /> }
+        { (isLoading || initialLoad) && <img src={logo} className="App-logo" alt="logo" /> }
 
-        <div className='checkbox-wrapper'>
-          <RenderCheckboxes
-            elementsArr={prefArray}
-            checkedObj={checkedPrefs}
-            callback={getPrefecturePopulation}
-            elementKey={'prefName'}
-          />
-        </div>
-
-        {!isLoading && 
-          <HighchartsReact
-            highcharts={Highcharts}
-            options={chartOptions}
-            ref={chartRef}
-          />
+        { initialLoad ? null :
+          (<React.Fragment>
+            <div className='checkbox-wrapper'>
+              <RenderCheckboxes
+                elementsArr={prefArray}
+                checkedObj={checkedPrefs}
+                callback={getPrefecturePopulation}
+                elementKey={'prefName'}
+              />
+            </div>
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={chartOptions}
+              ref={chartRef}
+            />
+          </React.Fragment>)
         }
         </header>
       </div>
---
+++ src/HighChartsOptions.js
@@ -0,0 +1,17 @@
+const chartOptions = {
+  credits: {
+    enabled: true,
+    text: 'Created by Christopher Button 2019'
+    },
+    title: { text: '', align: 'center', },
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle'
+  },
+  yAxis: { },
+  xAxis: { },
+  series: [ ],
+}
+
+export default chartOptions
